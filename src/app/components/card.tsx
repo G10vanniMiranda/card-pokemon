3561604a8b6445afc27656377b1f372e9e339afc
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-03-25 16:56:34 -0400

    update

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,7 +1,9 @@
+'use client'
 
 import Image from "next/image";
-import bulImg from "../../../public/bulbasaur.png";
 import Link from "next/link";
+import { useState } from "react";
+import { GiRoundStar } from "react-icons/gi";
 
 interface CardPokemonProps {
     codigo?: string;
@@ -9,20 +11,29 @@ interface CardPokemonProps {
     img: string;
     imgAlt: string;
     types: string;
+    favorite?: boolean;
 }
 export default function Card(props: CardPokemonProps) {
+
+    const [isFavorite, setFavorite] = useState("");
+    const handleFavorite = () => {
+        // setFavorite("text-yellow-500");
+        setFavorite((prev) => (prev === "text-yellow-500" ? "" : "text-yellow-500"));
+    }
     return (
         <div className="flex justify-center">
-            <Link href="/details">
-                <div className="w-96 h-96 bg-white">
 
-                    <div className="flex justify-end">
-                        <div className="bg-slate-400 w-20 h-10 flex items-center justify-center rounded-bl-2xl opacity">
-                            <h1 className="text-black"> {props.codigo} </h1>
-                        </div>
+            <div className="w-96 h-96 bg-white ">
+
+                <div className="flex justify-between items-center">
+                    <GiRoundStar size={40} className={isFavorite} />
+                    <div className="bg-slate-400 w-20 h-10 flex items-center justify-center rounded-bl-2xl opacity">
+                        <h1 className="text-black"> {props.codigo} </h1>
                     </div>
+                </div>
 
-                    <div className="flex justify-center items-center">
+                <Link href="/details">
+                    <div className="flex justify-center items-center ">
                         <Image
                             src={props.img}
                             alt={props.imgAlt}
@@ -31,17 +42,17 @@ export default function Card(props: CardPokemonProps) {
                             className="hover:scale-150 duration-300 cursor-pointer"
                         />
                     </div>
+                </Link>
 
-                    <div className="h-14 flex justify-center items-center gap-4">
-                        <div className="bg-green-600 w-20 flex justify-center rounded-lg p-1">{props.types}</div>
-                        <div className="bg-fuchsia-700 w-20 flex justify-center rounded-lg p-1">{props.types}</div>
-                    </div>
-
-                    <div className="bg-green-600 h-22 flex justify-center items-center">
-                        <h1 className="font-semibold text-4xl border-b-2 cursor-pointer">{props.name}</h1>
-                    </div>
+                <div className="h-14 flex justify-center items-center gap-4">
+                    <div className="bg-green-600 w-20 flex justify-center rounded-lg p-1">{props.types}</div>
+                    <div className="bg-fuchsia-700 w-20 flex justify-center rounded-lg p-1">{props.types}</div>
                 </div>
-            </Link>
+
+                <div className="bg-green-600 h-22 flex justify-center items-center" onClick={handleFavorite}>
+                    <h1 className="font-semibold text-4xl border-b-2 cursor-pointer">{props.name}</h1>
+                </div>
+            </div>
         </div>
     );
 }
